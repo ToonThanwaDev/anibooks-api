c4b220e8a8c8209865a68ab2a6d211752df4483a
ToonThanwaDev <toon.thanwa@hotmail.com>
2023-02-07 13:03:01 +0700

    feature get auth user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -56,3 +56,7 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getMe = (req, res, next) => {
+  res.status(200).json({ user: req.user });
+};
---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const createError = require("../utils/createError");
+const { User } = require("../models");
+
+module.exports = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith("Bearer ")) {
+      createError("You are unauthorized", 401);
+    }
+
+    const token = authorization.split(" ")[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findOne({
+      where: { id: payload.id },
+      attributes: {
+        exclude: ["password"]
+      }
+    });
+
+    if (!user) {
+      createError("You are unauthorized", 401);
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
